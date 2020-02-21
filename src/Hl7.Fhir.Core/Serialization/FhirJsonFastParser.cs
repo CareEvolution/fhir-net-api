ad8cf0782529ce1f4e54876724bbd68b65fb11d3
Scott Favre <favre@careevolution.com>
2020-02-21 11:53:38 -0500

    wip before generating

---
+++ src/Hl7.Fhir.Core/Serialization/FhirJsonFastParser.cs
@@ -0,0 +1,96 @@
+﻿using Hl7.Fhir.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Hl7.Fhir.Serialization
+{
+    public interface IFhirFastParser
+    {
+        Task<Model.Resource> ParseAsync(Stream stream, CancellationToken cancel);
+    }
+
+    public class FhirJsonFastParser : IFhirFastParser
+    {
+        private readonly ParserSource _source;
+        private readonly Model.Version _version;
+
+        public FhirJsonFastParser(Model.Version version)
+        {
+            _version = version;
+            _source = new JsonParserSource(version);
+        }
+
+        public async Task<Resource> ParseAsync(Stream stream, CancellationToken cancel)
+        {
+            using (var reader = new StreamReader(stream))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                string currentPropertyName = null;
+
+                while(await jsonReader.ReadAsync(cancel))
+                {
+                    switch (jsonReader.TokenType)
+                    {
+                        case JsonToken.None:
+                            break;
+                        case JsonToken.StartObject:
+                            _source.StartElement();
+                            break;
+                        case JsonToken.StartArray:
+                            _source.StartList();
+                            break;
+                        case JsonToken.StartConstructor:
+                            throw new NotSupportedException();
+                        case JsonToken.PropertyName:
+                            currentPropertyName = jsonReader.Value as string;
+                            break;
+                        case JsonToken.Comment:
+                            break;
+                        case JsonToken.Raw:
+                            throw new NotSupportedException();
+                        case JsonToken.String:
+                            if (currentPropertyName == "resourceType")
+                            {
+                                _source.SetResourceType((string)jsonReader.Value);
+                            }
+                            else
+                            {
+                                _source.Attribute(currentPropertyName, jsonReader.Value);
+                            }
+                            currentPropertyName = null;
+                            break;
+                        case JsonToken.Integer:
+                        case JsonToken.Float:
+                        case JsonToken.Date:
+                        case JsonToken.Boolean:
+                            _source.Attribute(currentPropertyName, (bool)jsonReader.Value);
+                            currentPropertyName = null;
+                            break;
+                        case JsonToken.Null:
+                        case JsonToken.Undefined:
+                            throw new NotSupportedException();
+                        case JsonToken.EndObject:
+                            _source.EndElement();
+                            break;
+                        case JsonToken.EndArray:
+                            _source.EndList();
+                            break;
+                        case JsonToken.EndConstructor:
+                            throw new NotSupportedException();
+                        case JsonToken.Bytes:
+                        default:
+                            throw new NotSupportedException();
+                    }
+                }
+
+                return _source.GetResource();
+            }
+        }
+    }
+}
---
+++ src/Hl7.Fhir.Core/Serialization/ParserSource.cs
@@ -0,0 +1,159 @@
+﻿using Hl7.Fhir.Model;
+using System;
+using System.Collections.Generic;
+
+namespace Hl7.Fhir.Serialization
+{
+    public interface IParserSource
+    {
+        TProp GetProperty<TProp>(string name);
+    }
+
+    public abstract class ParserSource : IParserSource
+    {
+        private readonly Model.Version _version;
+        private readonly Stack<State> _stack = new Stack<State>();
+        private State _current;
+        private Resource _resource;
+
+        protected ParserSource(Model.Version version)
+        {
+            _version = version;
+        }
+
+        public Model.Resource GetResource() => _resource;
+
+        public virtual void StartElement(string resourceType = null)
+        {
+            if (_current != null)
+            {
+                _stack.Push(_current);
+            }
+
+            _current = new ElementState
+            {
+                ResourceType = resourceType,
+            };
+        }
+
+        public virtual void SetResourceType(string resourceType)
+        {
+            var current = GetCurrent<ElementState>();
+            current.ResourceType = resourceType;
+        }
+
+        public virtual void EndElement()
+        {
+            var current = GetCurrent<ElementState>();
+
+            // create the resource;
+
+            if (_stack.Count == 0)
+            {
+                _resource = current.Resource;
+            }
+            else
+            {
+                var parent = _stack.Pop();
+                parent.AddResource(current.Resource);
+                _current = parent;
+            }
+        }
+
+        public virtual void StartList(string name)
+        {
+            var current = GetCurrent<ElementState>();
+
+            current.CurrentProperty = name;
+            _stack.Push(current);
+
+            _current = new ListState();
+        }
+
+        public virtual void EndList()
+        {
+            var current = GetCurrent<ListState>();
+
+            var parent = (_stack.Pop() as ElementState) ?? throw new InvalidOperationException("List cannot contain another list");
+
+            parent.AddList(current.List);
+
+            _current = parent;
+        }
+
+        public virtual void Attribute(string name, object value)
+        {
+            var current = GetCurrent<ElementState>();
+
+            current.Properties.Add(name, value);
+        }
+
+        TProp IParserSource.GetProperty<TProp>(string name)
+        {
+            var current = GetCurrent<ElementState>();
+
+            current.Properties.TryGetValue(name, out var value);
+
+            return (TProp)value;
+        }
+
+        private TState GetCurrent<TState>() where TState : State
+        {
+            if (_current != null) throw new InvalidOperationException("No current element");
+
+            return (_current as TState) ?? throw new InvalidOperationException($"Current state is {_current.GetType()}, not {typeof(TState)}");
+        }
+
+        private abstract class State
+        {
+            public abstract void AddResource(Resource resource);
+        }
+
+        private class ElementState : State
+        {
+            public Dictionary<string, object> Properties = new Dictionary<string, object>();
+
+            public string ResourceType;
+            public string CurrentProperty;
+            public Resource Resource;
+
+            public override void AddResource(Resource resource)
+            {
+                Properties.Add(CurrentProperty, resource);
+                CurrentProperty = null;
+            }
+
+            public void AddList(List<Resource> list)
+            {
+                Properties.Add(CurrentProperty, list);
+                CurrentProperty = null;
+            }
+        }
+
+        private class ListState : State
+        {
+            public List<Resource> List = new List<Resource>();
+
+            public override void AddResource(Resource resource)
+            {
+                List.Add(resource);
+            }
+        }
+    }
+
+    public class JsonParserSource : ParserSource
+    {
+        public JsonParserSource(Model.Version version) : base(version)
+        {
+
+        }
+    }
+
+    public class XmlParserSource : ParserSource
+    {
+        public XmlParserSource(Model.Version version) : base(version)
+        {
+
+        }
+    }
+}
